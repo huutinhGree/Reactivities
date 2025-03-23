43c534c85bacb575811017cf0c7d2be8173cba74
huutinhGree <trannhuutinh191922@gmail.com>
2025-03-23 15:29:39 +0700

    Initial React Skeleton

--- API/Program.cs
+++ API/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddDbContext<AppDbContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,7 +24,9 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+//-------MiddleWare
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000","https://localhost:3000"));
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
